f21f5e165c850c9639bb46da306d9101e0cf3da0
Rohansharma2358 <rohan.sharma.ug23@plaksha.edu.in>
2025-02-25 23:42:52 +0530

    dockerfile

--- distance_classification.py
+++ distance_classification.py
@@ -4,5 +4,5 @@ import pandas as pd
 def load_data(file_path):
     return pd.read_csv(file_path)
 
-if _name_ == "main":
+if __name__ == "__main__":
     print("Dataset loading script initialized.")
